63d62c3eb7180e707bbb6daee239c737669bc324
Pedro <pedrocpt2001@gmail.com>
2021-01-07 13:23:57 -0400

    Added shell classes for timer control

---
+++ SimpleTimerApp/Timer.swift
@@ -0,0 +1,16 @@
+//
+//  Timer.swift
+//  SimpleTimerApp
+//
+//  Created by Pedro Pagán on 1/7/21.
+//
+
+import Foundation
+
+
+struct Timer {
+    var name: String
+    var area: String
+    var timeElapsed: Double
+    
+}
---
+++ SimpleTimerApp/TimerManger.swift
@@ -0,0 +1,12 @@
+//
+//  TimerManger.swift
+//  SimpleTimerApp
+//
+//  Created by Pedro Pagán on 1/7/21.
+//
+
+import Foundation
+
+struct TimerManager {
+    var timers = [Timer]()
+}

68247a458a1911b865ade8b56e341d55c7e09bbc
Pedro <pedrocpt2001@gmail.com>
2021-01-07 13:08:27 -0400

    Initial Commit

---
+++ SimpleTimerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleTimerApp
+//
+//  Created by Pedro Pagán on 1/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
